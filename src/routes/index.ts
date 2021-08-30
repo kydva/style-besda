6084be680aa9cb4d189a6f121b112a21ce0b9371
kydva <kydvayv@gmail.com>
2021-08-30 17:05:26 +0300

    Add images caching

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,6 +12,7 @@ router.use('/pieces', piecesRouter);
 router.get('/img/:name', async (req, res) => {
     const data = await s3.getFile(req.params.name);
     if (data) {
+        res.setHeader('Cache-Control', 'max-age=' + 365 * 24 * 60 * 60);
         data.pipe(res);
     } else res.sendStatus(404);
 });
